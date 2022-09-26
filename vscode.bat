09189109f72e16957a9130a5714d9572876bf8ae
Rafale <rafale1128@gmail.com>
2022-09-27 02:21:13 +0900

    add VScode debug setting

--- vscode.bat
+++ vscode.bat
@@ -1,11 +1,4 @@
 @echo off
+set PICO_SDK_PATH=..\pico-sdk
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
-
-mkdir build 
-cd build
-cmake -G "NMake Makefiles" ..
-nmake
-
-copy /Y src\RGB_LED_TURN_SIGNAL.uf2 ..\
-
-cd ..\
+code -n .\
